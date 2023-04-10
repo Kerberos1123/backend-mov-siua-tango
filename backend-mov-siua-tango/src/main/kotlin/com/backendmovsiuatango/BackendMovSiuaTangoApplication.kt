b3eca8266f1a40795b006063904bb3a17f6fd303
Daniel Vargas <115358649+Kerberos1123@users.noreply.github.com>
2023-04-10 12:13:45 -0600

    Created base project with pring initializr

---
+++ backend-mov-siua-tango/src/main/kotlin/com/backendmovsiuatango/BackendMovSiuaTangoApplication.kt
@@ -0,0 +1,11 @@
+package com.backendmovsiuatango
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BackendMovSiuaTangoApplication
+
+fun main(args: Array<String>) {
+	runApplication<BackendMovSiuaTangoApplication>(*args)
+}
---
+++ backend-mov-siua-tango/src/test/kotlin/com/backendmovsiuatango/BackendMovSiuaTangoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.backendmovsiuatango
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BackendMovSiuaTangoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
