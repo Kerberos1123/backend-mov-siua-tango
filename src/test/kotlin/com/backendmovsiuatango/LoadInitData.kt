236f70869c54abdfb6d5554782d879207d68f321
Kerberos1123 <vargas.daniel111@gmail.com>
2023-04-24 12:37:52 -0600

    Test loadInitData

---
+++ src/test/kotlin/com/backendmovsiuatango/LoadInitData.kt
@@ -0,0 +1,26 @@
+package com.backendmovsiuatango
+
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.context.annotation.Profile
+import org.springframework.scheduling.config.Task
+import org.springframework.test.context.jdbc.Sql
+
+
+@Profile("initlocal")
+@SpringBootTest
+@Sql("/import-database.sql")
+
+
+class LoadInitData (
+    @Autowired
+    val taskRepository: TaskRepository,
+){
+    @Test
+    fun testTaskFindAll() {
+        val taskList: List<Task> = taskRepository.findAll()
+        Assertions.assertTrue(taskList.size == 2)
+    }
+}
---
+++ src/test/resources/import-databse.sql
@@ -0,0 +1,38 @@
+
+/** Priority **/
+INSERT INTO public.priority (id, label) VALUES (1, 'High');
+INSERT INTO public.priority (id, label) VALUES (2, 'Medium');
+INSERT INTO public.priority (id, label) VALUES (3, 'Low');
+
+/** Status **/
+INSERT INTO public.status (id, label) VALUES (1, 'Pending');
+INSERT INTO public.status (id, label) VALUES (2, 'In Progress');
+INSERT INTO public.status (id, label) VALUES (3, 'Done');
+
+/** Privilege **/
+INSERT INTO public.privilege (id, name) VALUES (1, 'WRITE_PRIVILEGE');
+INSERT INTO public.privilege (id, name) VALUES (2, 'READ_PRIVILEGE');
+
+
+
+/** Role **/
+INSERT INTO public.role (id, name) VALUES (1, 'ROLE_ADMIN');
+INSERT INTO public.role (id, name) VALUES (2, 'ROLE_PROF');
+INSERT INTO public.role (id, name) VALUES (3, 'ROLE_USER');
+
+/*
+
+/** Role-Privilege **/
+INSERT INTO public.role_privilege (role_id, privilege_id) VALUES (1, 1); /*Admin puede hacer read y write*/
+INSERT INTO public.role_privilege (role_id, privilege_id) VALUES (1, 2);
+INSERT INTO public.role_privilege (role_id, privilege_id) VALUES (2, 2);/*Prof solo read*/
+INSERT INTO public.role_privilege (role_id, privilege_id) VALUES (3, 2);/*User solo read*/
+
+
+/** Users **/
+INSERT INTO public.users (id, create_date, email, enabled, first_name, last_name, password, token_expired) VALUES (1, '2020-08-30 18:23:52.000000', 'admin@guzmanalan.com', true, 'Maikol', 'Guzman', '$2a$10$6hg/QTw8Th1EmYtg9/5HhOmRdg320A6J8DumNUqx.4AltXZAjp0VK', false);
+INSERT INTO public.users (id, create_date, email, enabled, first_name, last_name, password, token_expired) VALUES (2, '2022-04-16 08:23:25.000000', 'staff@guzmanalan.com', true, 'Staff', 'Guzman', '$2a$10$6hg/QTw8Th1EmYtg9/5HhOmRdg320A6J8DumNUqx.4AltXZAjp0VK', false);
+INSERT INTO public.users (id, create_date, email, enabled, first_name, last_name, password, token_expired) VALUES (3, '2022-04-16 08:24:48.000000', 'user@guzmanalan.com', true, 'User', 'Guzman', '$2a$10$6hg/QTw8Th1EmYtg9/5HhOmRdg320A6J8DumNUqx.4AltXZAjp0VK', false);
+
+
+*/
