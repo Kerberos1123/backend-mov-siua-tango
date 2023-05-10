add5b192809b46fce42e1c99009c9d69ca45e823
Kerberos1123 <vargas.daniel111@gmail.com>
2023-05-10 12:07:21 -0600

    Mappers Daniel

---
+++ src/main/kotlin/com/backendmovsiuatango/Mappers.kt
@@ -0,0 +1,44 @@
+package com.backendmovsiuatango
+
+import org.mapstruct.*
+import java.time.LocalDateTime
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+interface PriorityMapper {
+    fun priorityToPriorityDetails(
+        priority: Priority,
+    ): PriorityDetails
+
+    fun priorityListToPriorityDetailsList(
+        priorityList: List<Priority>,
+    ): List<PriorityDetails>
+}
+
+@Mapper(
+    imports = [LocalDateTime::class],
+    componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+interface TaskMapper {
+    fun taskToTaskResult(
+        task: Task,
+    ): TaskResult
+
+    fun taskListToTaskListResult(
+        taskList: List<Task>,
+    ): List<TaskResult>
+
+    @Mapping(target = "createDate", defaultExpression = "java(new java.util.Date())")
+    fun taskInputToTask(
+        taskInput: TaskInput,
+    ): Task
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    fun taskInputToTask(dto: TaskInput, @MappingTarget task: Task)
+}
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+interface RoleMapper {
+    fun roleListToRoleDetailsList(
+        roleList: Set<Role>?,
+    ): Set<RoleDetails>
+}
