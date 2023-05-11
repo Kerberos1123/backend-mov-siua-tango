b60d993368e77d4a39a0b9d8b9056f5c26b195b1
oKamix <126527588+NachoGreen@users.noreply.github.com>
2023-05-11 16:54:36 -0600

    Mappers actualizado

--- src/main/kotlin/com/backendmovsiuatango/Mappers.kt
+++ src/main/kotlin/com/backendmovsiuatango/Mappers.kt
@@ -42,3 +42,78 @@ interface RoleMapper {
         roleList: Set<Role>?,
     ): Set<RoleDetails>
 }
+
+//------------------------------------mappers nacho
+@Mapper(
+    imports = [LocalDateTime::class],
+    componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+interface TicketMapper{
+    fun ticketToTicketResult(
+        ticket:Ticket,
+    ):TicketResult
+
+    fun ticketListToTicketListResult(
+        ticketList: List<Ticket>,
+    ): List<TicketResult>
+
+    @Mapping(target = "createDate", defaultExpression = "java(new java.util.Date())")
+    fun ticketInputToTicket(
+        ticketInput: TicketInput,
+    ): Ticket
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    fun ticketInputToTicket(dto: TicketInput, @MappingTarget ticket: Ticket)
+
+}
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+interface TicketReasonMapper {
+    fun reasonListToReasonDetailsList(
+        reasonList: Set<TicketReason>?,
+    ): Set<TicketReasonDetails>
+}
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+interface AssetMapper {
+    fun assetListToAssetDetailsList(
+        assetList: Set<Asset>?,
+    ): Set<AssetDetails>
+}
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+interface AssetTypeMapper {
+    fun typeListToTypeDetailsList(
+        typeList: Set<AssetType>?,
+    ): Set<AssetTypeDetails>
+}
+
+@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE)
+interface RequestStateMapper {
+    fun stateListToStateDetailsList(
+        stateList: Set<RequestState>?,
+    ): Set<RequestStateDetails>
+}
+
+@Mapper(
+    imports = [LocalDateTime::class],
+    componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE
+)
+interface RequestMapper{
+    fun requestToRequestResult(
+        request:Request,
+    ):RequestResult
+
+    fun requestListToRequestListResult(
+        requestList: List<Request>,
+    ): List<RequestResult>
+
+    @Mapping(target = "createDate", defaultExpression = "java(new java.util.Date())")
+    fun requestInputToRequest(
+        requestInput: RequestInput,
+    ): Request
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    fun requestInputToRequest(dto: RequestInput, @MappingTarget request: Request)
+
+}
