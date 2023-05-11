3164beda412bb337b4149727ff039e9dbcf329f2
aragonj1112 <126797104+aragonj1112@users.noreply.github.com>
2023-05-11 14:14:09 -0600

    Cambios para postman
    Se agrego Webservice.kt

---
+++ src/main/kotlin/com/backendmovsiuatango/Webservice.kt
@@ -0,0 +1,95 @@
+package com.backendmovsiuatango
+
+import org.springframework.http.MediaType
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("\${url.priorities}")
+class PriorityController(private val priorityService: PriorityService){
+
+    /**
+     * WS to find all elements of type Priority
+     * @return A list of elements of type Priority
+     */
+    @GetMapping
+    @ResponseBody
+    fun findAll() = priorityService.findAll()
+
+    /**
+     * WS to find one Priority by the id
+     * @param id to find Priority
+     * @return the Priority found
+     */
+    @Throws(NoSuchElementException::class)
+    @GetMapping("{id}")
+    @ResponseBody
+    fun findById(@PathVariable id:Long) = priorityService.findById(id)
+}
+
+@RestController
+@RequestMapping("\${url.tasks}")
+class TaskController(private val taskService: TaskService) {
+    @GetMapping
+    @ResponseBody
+    fun findAll() = taskService.findAll()
+
+    @Throws(NoSuchElementException::class)
+    @GetMapping("{id}")
+    @ResponseBody
+    fun findById(@PathVariable id:Long) = taskService.findById(id)
+
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun create(@RequestBody taskInput: TaskInput) : TaskResult? {
+        return taskService.create(taskInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun update(@RequestBody taskInput: TaskInput) : TaskResult? {
+        return taskService.update(taskInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @DeleteMapping("{id}")
+    @ResponseBody
+    fun deleteById(@PathVariable id:Long) {
+        taskService.deleteById(id)
+    }
+}
+
+
+/*
+@RestController
+@RequestMapping("\${url.classes}")
+class ClassController(private val classService: ClassService) {
+    @GetMapping
+    @ResponseBody
+    fun findAll() = classService.findAll()
+
+    @Throws(NoSuchElementException::class)
+    @GetMapping("{id}")
+    @ResponseBody
+    fun findById(@PathVariable id:Long) = classService.findById(id)
+
+    @PostMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun create(@RequestBody classInput: ClassInput) : ClassResult? {
+        return classService.create(classInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @PutMapping(consumes = [MediaType.APPLICATION_JSON_VALUE], produces = [MediaType.APPLICATION_JSON_VALUE])
+    @ResponseBody
+    fun update(@RequestBody classInput: ClassInput) : ClassResult? {
+        return classService.update(classInput)
+    }
+
+    @Throws(NoSuchElementException::class)
+    @DeleteMapping("{id}")
+    @ResponseBody
+    fun deleteById(@PathVariable id:Long) {
+        classService.deleteById(id)
+    }
+}*/
